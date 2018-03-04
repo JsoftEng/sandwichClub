95faa2954ce70b4936e337a7e817b1b4d040f5a3
John Johnson <jsofteng@gmail.com>
2018-03-03 23:50:51 -0500

    began project

---
+++ app/src/main/java/com/github/jsofteng/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,13 @@
+package com.github.jsofteng.sandwichclub.utils;
+
+import com.github.jsofteng.sandwichclub.model.Sandwich;
+import java.
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        Sandwich sandwich = new Sandwich();
+
+        return sandwich;
+    }
+}

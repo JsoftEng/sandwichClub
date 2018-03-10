6009c9181a95ee4e51883e11bd892030a480292f
John Johnson <jsofteng@gmail.com>
2018-03-10 02:14:25 -0500

    finished up project 1

--- app/src/main/java/com/github/jsofteng/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/github/jsofteng/sandwichclub/DetailActivity.java
@@ -3,17 +3,26 @@ package com.github.jsofteng.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.github.jsofteng.sandwichclub.model.Sandwich;
 import com.github.jsofteng.sandwichclub.utils.JsonUtils;
 
+import java.io.IOException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    TextView mSandwichOrigin;
+    TextView mSandwichAlsoKnownAs;
+    TextView mSandwichIngredients;
+    TextView mSandwichDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mSandwichOrigin = findViewById(R.id.origin_tv);
+        mSandwichDescription = findViewById(R.id.description_tv);
+        mSandwichAlsoKnownAs = findViewById(R.id.also_known_tv);
+        mSandwichIngredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,19 +49,25 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
-            // Sandwich data unavailable
+        try {
+            Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+            if (sandwich == null) {
+                // Sandwich data unavailable
+                closeOnError();
+                return;
+            }
+
+            populateUI(sandwich);
+            Picasso.with(this)
+                    .load(sandwich.getImage())
+                    .into(ingredientsIv);
+
+            setTitle(sandwich.getMainName());
+        }catch(IOException ioe){
             closeOnError();
             return;
         }
-
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +75,33 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    //Populate views, set invisible if data does not exist
+    private void populateUI(Sandwich sandwich) {
+        if (sandwich.getPlaceOfOrigin() != null){
+            mSandwichOrigin.setText(sandwich.getPlaceOfOrigin());
+            mSandwichOrigin.setVisibility(View.VISIBLE);
+        }else{
+            mSandwichOrigin.setVisibility(View.INVISIBLE);
+        }
 
+        if(sandwich.getDescription() != null){
+            mSandwichDescription.setText(sandwich.getDescription());
+            mSandwichDescription.setVisibility(View.VISIBLE);
+        }else{
+            mSandwichDescription.setVisibility(View.INVISIBLE);
+        }
+
+        if (sandwich.getIngredients() != null){
+            mSandwichIngredients.setVisibility(View.VISIBLE);
+            mSandwichIngredients.setText(android.text.TextUtils.join(",",sandwich.getIngredients()));
+        }else{
+            mSandwichIngredients.setVisibility(View.INVISIBLE);
+        }
+        if (sandwich.getAlsoKnownAs() != null){
+            mSandwichAlsoKnownAs.setVisibility(View.VISIBLE);
+            mSandwichAlsoKnownAs.setText(android.text.TextUtils.join(",",sandwich.getAlsoKnownAs()));
+        }else{
+            mSandwichAlsoKnownAs.setVisibility(View.INVISIBLE);
+        }
     }
 }
--- app/src/main/java/com/github/jsofteng/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/github/jsofteng/sandwichclub/utils/JsonUtils.java
@@ -1,13 +1,77 @@
 package com.github.jsofteng.sandwichclub.utils;
 
 import com.github.jsofteng.sandwichclub.model.Sandwich;
-import java.
+
+import android.util.JsonReader;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
+    public static Sandwich parseSandwichJson(String json) throws IOException {
+        InputStream is = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
+        JsonReader reader = new JsonReader(new InputStreamReader(is,"UTF-8"));
 
-    public static Sandwich parseSandwichJson(String json) {
+        return readSandwich(reader);
+    }
+
+    private static Sandwich readSandwich(JsonReader reader)throws IOException{
         Sandwich sandwich = new Sandwich();
+        String data;
+
+        reader.beginObject();
+        while (reader.hasNext()){
+            String name = reader.nextName();
+            if (name.equals("mainName")){
+                data = reader.nextString();
+                if(data.length() > 0){
+                    sandwich.setMainName(data);
+                }else{
+                    sandwich.setMainName("Not Available");
+                }
+            }else if(name.equals("alsoKnownAs")) {
+                sandwich.setAlsoKnownAs(readStringArray(reader));
+            }else if(name.equals("placeOfOrigin")){
+                data = reader.nextString();
+                if (data.length() > 0) {
+                    sandwich.setPlaceOfOrigin(data);
+                }else{
+                    sandwich.setPlaceOfOrigin("Not Available");
+                }
+            }else if(name.equals("description")) {
+                data = reader.nextString();
+                if(data.length() > 0) {
+                    sandwich.setDescription(data);
+                }else{
+                    sandwich.setDescription("Not Available");
+                }
+            }else if(name.equals("image")){
+                sandwich.setImage(reader.nextString());
+            }else if(name.equals("ingredients")){
+                sandwich.setIngredients(readStringArray(reader));
+            }else{
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
 
         return sandwich;
+    }
+
+    private static List<String> readStringArray(JsonReader reader) throws IOException{
+        List<String> strings = new ArrayList<>();
+
+        reader.beginArray();
+        while (reader.hasNext()){
+            strings.add(reader.nextString());
+        }
+        reader.endArray();
+
+        return strings;
     }
 }
